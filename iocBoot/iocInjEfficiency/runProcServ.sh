1e4281d1ef983ab598735b558b3bc9c848b99b12
andrei_pereira <andrei.pereira@lnls.br>
2018-04-26 15:00:58 -0300

    iocBoot/iocInjEfficiency/{parseCMDOpts.sh, run*}: Add option for procServ telnet port

--- iocBoot/iocInjEfficiency/runProcServ.sh
+++ iocBoot/iocInjEfficiency/runProcServ.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
+set -e
+set +u
+
+# Parse command-line options
+. ./parseCMDOpts.sh "$@"
+
+# Use defaults if not set
+if [ -z "${DEVICE_TELNET_PORT}" ]; then
+   DEVICE_TELNET_PORT="20000"
+fi
+
+if [ -z "${INJ_EFFICIENCY_INSTANCE}" ]; then
+   INJ_EFFICIENCY_INSTANCE="InjEfficiency_1"
+fi
+
+set -u
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n InjEfficiency -i ^C^D 20000 ./runInjEfficiency.sh "$@"
+/usr/local/bin/procServ -f -n ${INJ_EFFICIENCY_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runInjEfficiency.sh "$@"
